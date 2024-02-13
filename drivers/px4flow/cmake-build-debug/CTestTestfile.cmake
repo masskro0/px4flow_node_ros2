8653234b2ed9fa642c1c43976b290f065b71ac8b
Michael Heine <michael.heine@fau.de>
2024-02-13 22:14:30 +0100

    removed debug folder

--- drivers/px4flow/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/masskro/Masterarbeit/src/px4flow
-# Build directory: /home/masskro/Masterarbeit/src/px4flow/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
